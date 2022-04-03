a42676bfdda7d340b855266b2765bbef091020a1
Colin McLear <mclear@fastmail.com>
2022-04-03 18:50:24 -0400

    First commit
    - Add files

---
+++ lambda-line.el
@@ -0,0 +1,38 @@
+;;; lambda-line.el --- A custom status line  -*- lexical-binding: t -*-
+
+;; Author: Colin McLear
+;; Maintainer: Colin McLear
+;; Version: 0.1
+;; Package-Requires: ((emacs "27.1"))
+;; Homepage: https://github.com/Lambda-Emacs/lambda-line
+;; Keywords: mode-line faces
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; lambda-line is a minimal, though opinionated, status line (i.e. "mode line")
+;; for use as either header or footer in a buffer.
+
+;;; Code:
+
+
+;;; Provide:
+(provide 'lambda-line)
+
+;;; lambda-line.el ends here
